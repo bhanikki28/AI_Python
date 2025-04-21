be154f331ee30db79b7ffe08f881860a9b56b80e
Anandha Janani <anandhajanani@Anandhas-MacBook-Air.local>
2025-04-21 18:48:03 +1000

    fixing the new category button

--- FinanceApp/main.py
+++ FinanceApp/main.py
@@ -7,6 +7,25 @@ import os
 st.set_page_config(page_title="Simple Finance App", page_icon=" 💰", layout = "wide")
 
 
+category_file = "categories.json"
+
+if "categories" not in st.session_state:
+    st.session_state.categories = {
+        "Uncategorized" : []
+    }
+
+
+if os.path.exists(category_file):
+    with open("categories.json","r") as f:
+        st.session_state.categories = json.load(f)
+
+
+def save_categories():
+    with open(category_file, "w") as f:
+        json.dump(st.session_state.categories, f)
+
+
+
 def load_transactions(transaction_file):
     print(f"Reading the transaction file : {transaction_file.name}")
     try:
@@ -33,6 +52,16 @@ def main():
             credits_df = df[df["Debit/Credit"] == "Credit"].copy()
             tab1, tab2 = st.tabs( ["Expenses(Debits)", "Payments(Credits)"])
             with tab1:
+                new_category = st.text_input("New Category Name")
+                add_button = st.button("Add Category")
+
+                if add_button and new_category:
+                    if new_category not in st.session_state.categories:
+                        st.session_state.categories[new_category] = []
+                        save_categories()
+                        st.success(f"Added New Category : {new_category}")
+                        st.rerun()
+
                 st.write(debits_df)
             with tab2:
                 st.write(credits_df)
