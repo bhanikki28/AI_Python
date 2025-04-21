69a0b313e912249da1a7139521a0936a3893ff7e
Anandha Janani <anandhajanani@Anandhas-MacBook-Air.local>
2025-04-21 12:12:31 +1000

    adding tabs in the dasboard to filter out debit/credit transactions

--- FinanceApp/main.py
+++ FinanceApp/main.py
@@ -14,7 +14,7 @@ def load_transactions(transaction_file):
         #df.columns = [ col.strip for col in df.columns]
         df["Amount"] = df["Amount"].str.replace(",","").astype(float)
         df["Date"] = pd.to_datetime(df["Date"], format="%d %b %Y")
-        st.write(df)
+        #st.write(df)
         return df
     except Exception as e:
         st.error(f"Error processing the file : {str(e)}")
@@ -28,5 +28,14 @@ def main():
 
     if uploaded_file is not None:
         df = load_transactions(uploaded_file)
+
+        if df is not None:
+            debits_df = df[df["Debit/Credit"] == "Debit"].copy()
+            credits_df = df[df["Debit/Credit"] == "Credit"].copy()
+            tab1, tab2 = st.tabs( ["Expenses(Debits)", "Payments(Credits)"])
+            with tab1:
+                st.write(debits_df)
+            with tab2:
+                st.write(credits_df)
 
 main()   
